d1ee017b624a176a9e163fa0eafc641b1d55b47f
wanlin000 <93826525+wanlin000@users.noreply.github.com>
2025-03-25 11:26:04 +0800

    Update CIKList2.sol

--- CIKList2.sol
+++ CIKList2.sol
@@ -5,18 +5,17 @@ contract CIKList {
     address public TA;
 
     struct Uint512 {
-        uint256 high;
+        uint256 high; //用256位的高位和256位的低位，表示512位的证书和公钥（由于solidity最大只能支持256位无符号整数）
         uint256 low;
     }
 
     struct CIK {
-        Uint512 CIK1;
+        Uint512 CIK1;  //两个证书
         Uint512 CIK2;
     }
     
     struct APK {
-        Uint512 APK1;
-        Uint512 APK2;
+        Uint512 APK1;  //一个公钥
     }
 
     mapping(uint256 => CIK) cik;
@@ -34,15 +33,15 @@ contract CIKList {
         return msg.sender;
     }
 
-    function Update(Uint512 memory APK1, Uint512 memory APK2, uint256 txid) public returns (bool) {
+    function Update(Uint512 memory APK1, uint256 txid) public returns (bool) {
         require(msg.sender == TA, "Only the TA can update the mapping");
-        uint256 index = uint256(keccak256(abi.encode(APK1.high, APK1.low, APK2.high, APK2.low)));
+        uint256 index = uint256(keccak256(abi.encode(APK1.high, APK1.low)));
         apkToTxid[index] = txid;
         return true;
     }
     
-    function GetTxid(Uint512 memory APK1, Uint512 memory APK2) public view returns (uint256) {
-        uint256 index = uint256(keccak256(abi.encode(APK1.high, APK1.low, APK2.high, APK2.low)));
+    function GetTxid(Uint512 memory APK1) public view returns (uint256) {
+        uint256 index = uint256(keccak256(abi.encode(APK1.high, APK1.low)));
         return apkToTxid[index];
     }
 
@@ -66,9 +65,9 @@ contract CIKList {
         return false;
     }
 
-    function DeleteMapping(Uint512 memory APK1, Uint512 memory APK2) public returns (bool) {
+    function DeleteMapping(Uint512 memory APK1) public returns (bool) {
         require(msg.sender == TA, "Only the TA can delete the APK-to-txid mapping");
-        uint256 index = uint256(keccak256(abi.encode(APK1.high, APK1.low, APK2.high, APK2.low)));
+        uint256 index = uint256(keccak256(abi.encode(APK1.high, APK1.low)));
         if (apkToTxid[index] != 0) {
             delete apkToTxid[index];
             return true;
